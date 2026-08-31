f4b7bd8b3d6af14f252cce60ffd35786ff05b5bb
devsiteua <107812007+devsiteua@users.noreply.github.com>
2026-08-31 03:52:14 +0100

    feat: fetch and display notes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchNotes } from '../../services/noteService';
+import NoteList from '../NoteList/NoteList';
 import css from './App.module.css';
 
 export default function App() {
+  const { data } = useQuery({
+    queryKey: ['notes'],
+    queryFn: () =>
+      fetchNotes({
+        page: 1,
+        perPage: 12,
+      }),
+  });
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}></header>
+
+      {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
     </div>
   );
 }
