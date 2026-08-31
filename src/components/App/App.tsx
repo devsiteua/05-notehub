d2d7c10ddbf243be620c3f24dd7141670f5c4667
devsiteua <107812007+devsiteua@users.noreply.github.com>
2026-08-31 15:18:47 +0100

    feat: add notes pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,36 @@
-import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { fetchNotes } from '../../services/noteService';
 import NoteList from '../NoteList/NoteList';
+import Pagination from '../Pagination/Pagination';
 import css from './App.module.css';
 
+const PER_PAGE = 12;
+
 export default function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+
   const { data } = useQuery({
-    queryKey: ['notes'],
+    queryKey: ['notes', currentPage],
     queryFn: () =>
       fetchNotes({
-        page: 1,
-        perPage: 12,
+        page: currentPage,
+        perPage: PER_PAGE,
       }),
+    placeholderData: keepPreviousData,
   });
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}></header>
+
+      {data && data.totalPages > 1 && (
+        <Pagination
+          pageCount={data.totalPages}
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+        />
+      )}
 
       {data && data.notes.length > 0 && <NoteList notes={data.notes} />}
     </div>
